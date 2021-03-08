c2356df998f5b6c596333298f92357c6c42be13e
Juliano Cardoso <cardoso.juliano@gmail.com>
2021-03-08 00:54:05 -0300

    Criação do componente Guard

---
+++ src/app/guards/authentication.guard.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthService } from '../services/authentication/auth.service';
+import { Token } from '../users/user/shared/token.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthenticationGuard implements CanActivate {
+  /**
+   *
+   */
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) {}
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    console.log('this.authService.getToken() = ', this.authService.getToken());
+
+    const res = this.authService.getToken().then((result) => (result?.token != null));
+    if (res) {
+      this.router.navigate(['/login']);
+    }
+
+    return res;
+  }
+
+}
