1ef97f89ef57e4222d54af8184fb66e66186f8d4
Juliano Cardoso <cardoso.juliano@gmail.com>
2021-03-13 00:33:01 -0300

    Verificação de rotas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from './services/authentication/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'dashboard-app';
+
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ){
+    this.routeVerify();
+  }
+
+  async routeVerify() {
+    const res = await this.authService.getToken().then((result) => (result?.access_token != null));
+    if (!res) {
+      this.router.navigate(['/login']);
+    }
+  }
 }
