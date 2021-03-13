4ae31b64290f0357499bc53bd251ce4cf3603e99
Juliano Cardoso <cardoso.juliano@gmail.com>
2021-03-13 00:43:03 -0300

    Aplicação de injeção de dependência no user.service

--- src/app/users/user/shared/user.service.ts
+++ src/app/users/user/shared/user.service.ts
@@ -2,20 +2,20 @@ import { Injectable } from '@angular/core';
 import { ResourceService } from 'src/app/api/resource.service';
 import { Api } from 'src/app/api/api.service';
 import { LocalStorage } from '@ngx-pwa/local-storage';
-import { HttpHeaders } from '@angular/common/http';
+import { HttpHeaderService } from 'src/app/api/httpHeader.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService extends ResourceService<any> {
   protected endpoint = 'users';
-  protected headers = new HttpHeaders().set('Content-Type', 'application/x-www-form-urlencoded')
 
   constructor(
-    protected http: Api,
-    protected localStorage: LocalStorage
+    protected api: Api,
+    protected localStorage: LocalStorage,
+    protected httpHeaderService: HttpHeaderService
   ) {
-    super(http, localStorage);
+    super(api, localStorage, httpHeaderService);
   }
 
 
