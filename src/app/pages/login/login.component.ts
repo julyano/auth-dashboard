32d4fe143f2324e73587495550941e8f93feed0e
Juliano Cardoso <cardoso.juliano@gmail.com>
2021-03-13 00:38:57 -0300

    Criação das páginas de home, signup e login

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,15 +1,73 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { LocalStorage } from '@ngx-pwa/local-storage';
+import { AuthService } from 'src/app/services/authentication/auth.service';
+import { User } from 'src/app/users/user/shared/user.model';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent implements OnInit{
 
-  constructor() { }
+  private user: User = new User;
+  loginform!: FormGroup;
+  username!: FormControl;
+  password!: FormControl;
+  isSubmiting = false;
 
-  ngOnInit(): void {
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private localStorage: LocalStorage) { }
+
+  async ngOnInit() {
+    this.createFormControls();
+    this.createForm();
+
+    if (this.localStorage.getItem('AuthToken') !== null) {
+      this.router.navigate(['/home']);
+    }
   }
 
+  createFormControls() {
+    this.username = new FormControl('', Validators.required);
+    this.password = new FormControl('', Validators.required);
+    /*this.password = new FormControl('', [
+      Validators.required,
+      Validators.minLength(8)
+    ]);*/
+    /*this.email = new FormControl('', [
+      Validators.required,
+      Validators.pattern("[^ @]*@[^ @]*")
+    ]);*/
+  }
+
+  createForm() {
+    this.loginform = new FormGroup({
+      username: this.username,
+      password: this.password
+    });
+  }
+
+
+  async login() {
+    if (this.loginform.valid) {
+      console.log("Form Submitted!");
+      this.loginform.disable();
+      this.isSubmiting = true;
+      this.user.username = this.username.value;
+      this.user.password = this.password.value;
+      console.log('user = ', this.user);
+
+
+      await this.authService.login(this.user).then(() => {
+        this.loginform.enable();
+        this.isSubmiting = false;
+      });
+      this.loginform.reset();
+    }
+  }
 }
--- src/app/pages/signup/signup.component.ts
+++ src/app/pages/signup/signup.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { LocalStorage } from '@ngx-pwa/local-storage';
+import { AuthService } from 'src/app/services/authentication/auth.service';
+import { User } from 'src/app/users/user/shared/user.model';
+import { UserService } from 'src/app/users/user/shared/user.service';
 
 @Component({
   selector: 'app-signup',
@@ -7,9 +13,68 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupComponent implements OnInit {
 
-  constructor() { }
+  private user: User = new User;
+  registerForm!: FormGroup;
+  username!: FormControl;
+  password!: FormControl;
+  confirmPassword!: FormControl;
+  email!: FormControl;
+  isSubmiting = false;
 
-  ngOnInit(): void {
+  constructor(
+    private userService: UserService,
+    private router: Router) { }
+
+  async ngOnInit() {
+    this.createFormControls();
+    this.createForm();
   }
 
+  createFormControls() {
+    this.username = new FormControl('', Validators.required);
+    this.password = new FormControl('', Validators.required);
+    this.confirmPassword = new FormControl('', Validators.required);
+    this.email = new FormControl('', [
+      Validators.required,
+      Validators.pattern("[^ @]*@[^ @]*")
+    ]);
+    /*this.password = new FormControl('', [
+      Validators.required,
+      Validators.minLength(8)
+    ]);*/
+  }
+
+  createForm() {
+    this.registerForm = new FormGroup({
+      username: this.username,
+      email: this.email,
+      password: this.password,
+      confirmPassword: this.confirmPassword
+    });
+  }
+
+
+  async register() {
+    if (this.registerForm.valid) {
+      this.registerForm.disable();
+      this.isSubmiting = true;
+      console.log("Form Submitted!");
+      this.user.username = this.username.value;
+      this.user.email = this.email.value;
+      this.user.password = this.password.value;
+
+      console.log('user = ', this.user);
+
+
+      await this.userService.create(this.user).toPromise().then((result) => {
+        console.log('res = ', result);
+
+        this.registerForm.reset();
+        this.registerForm.enable();
+        this.isSubmiting = false;
+      });
+
+      this.router.navigate(["/login"]);
+    }
+  }
 }
